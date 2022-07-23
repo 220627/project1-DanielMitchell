b5c7590b6672125d960864f1a3d2d0d2ca9375b1
Daniel Mitchell <mitchdan@mail.gvsu.edu>
2022-07-23 04:25:05 -0400

    completed models and added javalin connectivity. successfully connected to database

--- P1Template/src/main/java/com/revature/Launcher.java
+++ P1Template/src/main/java/com/revature/Launcher.java
@@ -1,27 +1,32 @@
 package com.revature;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.revature.daos.ReimbursementDAO;
+import com.revature.daos.UserDAO;
+import com.revature.utils.ConnectionUtil;
+
+import io.javalin.Javalin;
+
 public class Launcher {
 
 	public static void main(String[] args) {
 		
-		//Welcome to P1! 
+		UserDAO uDAO = new UserDAO();
+		ReimbursementDAO rDAO = new ReimbursementDAO();
 		
-		//If you're reading this, you've successfully cloned your repo and imported the template
+		System.out.println("========Welcome to the Employee Reimbursement Management System (ERS)===========");
 		
-		//Do you coding in this project, and don't forget to save/push your progress with:
-		//git add.
-		//git commit -m"message"
-		//git push
-		
-		//yes, you WILL need to push to your repo. The clients will want to see your project repos in your portfolios.
-		
-		//here's a dog to help you on your way. Have fun!
-		
-//               __
-//          (___()'`;
-//          /,    /`
-//          \\"--\\
-		
+		try(Connection conn = ConnectionUtil.getConnection()){
+			System.out.println("CONNECTION SUCCESSFUL :)");
+		} catch (SQLException e) {
+			System.out.println("CONNECTION FAILED...");
+			e.printStackTrace();
+		} //End of the connection test
+			
+		Javalin app = Javalin.create(config -> {
+					config.enableCorsForAllOrigins();
+				}).start(3000); 
 	}
-	
-}
+}//End of Main Method
---
+++ P1Template/src/main/java/com/revature/models/Reimbursement.java
@@ -0,0 +1,86 @@
+package com.revature.models;
+
+public class Reimbursement {
+	
+	private int reimb_id;
+	private int reimb_amount;
+	private int reimb_submitted; // <--------------- CHECK TIMESTAMP NOTES
+	private String reimb_description;
+	private int reimb_author;
+	private int reimb_resolver;
+	private ReimbursementStatus reimbuirsement_status;
+	private ReimbursementType reimbursement_type;
+	
+	
+	public Reimbursement() {
+		super();
+	}
+	public Reimbursement(int reimb_id, int reimb_amount, int reimb_submitted, String reimb_description,
+			int reimb_author, int reimb_resolver, ReimbursementStatus reimbuirsement_status,
+			ReimbursementType reimbursement_type) {
+		super();
+		this.reimb_id = reimb_id;
+		this.reimb_amount = reimb_amount;
+		this.reimb_submitted = reimb_submitted;
+		this.reimb_description = reimb_description;
+		this.reimb_author = reimb_author;
+		this.reimb_resolver = reimb_resolver;
+		this.reimbuirsement_status = reimbuirsement_status;
+		this.reimbursement_type = reimbursement_type;
+	}
+	@Override
+	public String toString() {
+		return "Reimbursement [reimb_id=" + reimb_id + ", reimb_amount=" + reimb_amount + ", reimb_submitted="
+				+ reimb_submitted + ", reimb_description=" + reimb_description + ", reimb_author=" + reimb_author
+				+ ", reimb_resolver=" + reimb_resolver + ", reimbuirsement_status=" + reimbuirsement_status
+				+ ", reimbursement_type=" + reimbursement_type + "]";
+	}
+	public int getReimb_id() {
+		return reimb_id;
+	}
+	public void setReimb_id(int reimb_id) {
+		this.reimb_id = reimb_id;
+	}
+	public int getReimb_amount() {
+		return reimb_amount;
+	}
+	public void setReimb_amount(int reimb_amount) {
+		this.reimb_amount = reimb_amount;
+	}
+	public int getReimb_submitted() {
+		return reimb_submitted;
+	}
+	public void setReimb_submitted(int reimb_submitted) {
+		this.reimb_submitted = reimb_submitted;
+	}
+	public String getReimb_description() {
+		return reimb_description;
+	}
+	public void setReimb_description(String reimb_description) {
+		this.reimb_description = reimb_description;
+	}
+	public int getReimb_author() {
+		return reimb_author;
+	}
+	public void setReimb_author(int reimb_author) {
+		this.reimb_author = reimb_author;
+	}
+	public int getReimb_resolver() {
+		return reimb_resolver;
+	}
+	public void setReimb_resolver(int reimb_resolver) {
+		this.reimb_resolver = reimb_resolver;
+	}
+	public ReimbursementStatus getReimbuirsement_status() {
+		return reimbuirsement_status;
+	}
+	public void setReimbuirsement_status(ReimbursementStatus reimbuirsement_status) {
+		this.reimbuirsement_status = reimbuirsement_status;
+	}
+	public ReimbursementType getReimbursement_type() {
+		return reimbursement_type;
+	}
+	public void setReimbursement_type(ReimbursementType reimbursement_type) {
+		this.reimbursement_type = reimbursement_type;
+	}
+}//End of Reimbursement
